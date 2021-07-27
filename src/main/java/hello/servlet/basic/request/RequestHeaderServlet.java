c55846986e70c115423951c0fcec17e98ad1967d
donkim <n011@nate.com>
2021-07-27 12:00:35 +0900

    commit

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -13,6 +14,7 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
+        printHeaders(request);
     }
 
     private void printStartLine(HttpServletRequest request) {
@@ -25,6 +27,17 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getQueryString() = " + request.getQueryString());
         System.out.println("request.isSecure() = " + request.isSecure());
         System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    private void printHeaders(HttpServletRequest request){
+        System.out.println("--- headers - start ---");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println( headerName + " : " + headerName);
+        }
+        System.out.println("--- headers - end ---");
         System.out.println();
     }
 }
